278da29a35819bffd8b8e222df96362302802d42
Anurag Koul <koulanurag@gmail.com>
2018-08-14 14:47:58 -0700

    commented explainations in hra for scaii

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Adaptation Based Programming
 ## Installation
 ```bash
    cd abp
-   pip3 install -r requirement.txt
+   pip3 install -r requirements.txt
    python setup.py install
 ```
 
@@ -31,7 +31,7 @@ Adaptation Based Programming
     |--test-episodes TEST_EPISODES| Set the number of test episodes|
     |--decay-steps DECAY_STEPS | Set the decay rate for exploration|
 
-    Minimal Example:
+* Minimal Example:
     ```bash
     python -m abp.trainer.task_runner -f tasks/fruit_collection/hra/v1 -t abp.examples.open_ai.fruit_collection.hra -r
     ```
--- abp/examples/scaii/tower_capture/hra.py
+++ abp/examples/scaii/tower_capture/hra.py
@@ -6,13 +6,13 @@ import logging
 
 logger = logging.getLogger('root')
 
-
 import tensorflow as tf
 import numpy as np
 
 from abp import HRAAdaptive
-from abp.utils import clear_summary_path, Explanation, saliency_to_excel
-from abp.utils.histogram import MultiQHistogram
+# from abp.utils import clear_summary_path, Explanation, saliency_to_excel
+from abp.utils import clear_summary_path, saliency_to_excel
+# from abp.utils.histogram import MultiQHistogram
 
 from scaii.env.sky_rts.env.scenarios.tower_example import TowerExample
 from scaii.env.explanation import Explanation as SkyExplanation, BarChart, BarGroup, Bar
@@ -36,18 +36,17 @@ def run_task(evaluation_config, network_config, reinforce_config):
     choice_descriptions = list(map(lambda x: x[0], actions))
     choices = list(map(lambda x: x[1], actions))
 
-    choose_tower = HRAAdaptive(name = "Tower",
-                               choices = choices,
-                               reward_types = reward_types,
-                               network_config = network_config,
-                               reinforce_config = reinforce_config)
-
+    choose_tower = HRAAdaptive(name="Tower",
+                               choices=choices,
+                               reward_types=reward_types,
+                               network_config=network_config,
+                               reinforce_config=reinforce_config)
 
     training_summaries_path = evaluation_config.summaries_path + "/train"
     clear_summary_path(training_summaries_path)
     train_summary_writer = tf.summary.FileWriter(training_summaries_path)
 
-    #Training Episodes
+    # Training Episodes
     for episode in range(evaluation_config.training_episodes):
         state = env.reset()
         total_reward = 0
@@ -70,7 +69,7 @@ def run_task(evaluation_config, network_config, reinforce_config):
         choose_tower.end_episode(state.state)
 
         logger.info("Episode %d : %d, Step: %d" % (episode + 1, total_reward, step))
-        episode_summary.value.add(tag = "Train/Reward", simple_value = total_reward)
+        episode_summary.value.add(tag="Train/Reward", simple_value=total_reward)
         train_summary_writer.add_summary(episode_summary, episode + 1)
 
     train_summary_writer.flush()
@@ -81,14 +80,13 @@ def run_task(evaluation_config, network_config, reinforce_config):
     clear_summary_path(test_summaries_path)
     test_summary_writer = tf.summary.FileWriter(test_summaries_path)
 
-
-    #Test Episodes
+    # Test Episodes
     for episode in range(evaluation_config.test_episodes):
         contrastive = True
-        explanation = SkyExplanation("Tower Capture", (40,40))
+        explanation = SkyExplanation("Tower Capture", (40, 40))
         layer_names = ["HP", "Agent Location", "Small Towers", "Big Towers", "Friend", "Enemy"]
 
-        adaptive_explanation = Explanation(choose_tower)
+        #adaptive_explanation = Explanation(choose_tower)
 
         state = env.reset(visualize=evaluation_config.render, record=True)
         total_reward = 0
@@ -97,19 +95,19 @@ def run_task(evaluation_config, network_config, reinforce_config):
         while not state.is_terminal():
             tower_to_kill, q_values = choose_tower.predict(state.state)
             combined_q_values = np.sum(q_values, axis=0)
-            saliencies = adaptive_explanation.generate_saliencies(state.state, contrastive)
+            #saliencies = adaptive_explanation.generate_saliencies(state.state, contrastive)
             charts = []
 
             decomposed_q_chart = BarChart("Q Values", "Actions", "QVal By Reward Type")
             for choice_idx, choice in enumerate(choices):
                 key = choice_descriptions[choice_idx]
-                explanation.add_layers(layer_names, saliencies[choice]["all"], key = key)
-                group = BarGroup("Attack {}".format(key), saliency_key = key)
+                # explanation.add_layers(layer_names, saliencies[choice]["all"], key = key)
+                group = BarGroup("Attack {}".format(key), saliency_key=key)
 
                 for reward_index, reward_type in enumerate(reward_types):
                     key = "{}_{}".format(choice, reward_type)
-                    bar = Bar(reward_type, q_values[reward_index][choice_idx], saliency_key = key)
-                    explanation.add_layers(layer_names, saliencies[choice][reward_type], key=key)
+                    bar = Bar(reward_type, q_values[reward_index][choice_idx], saliency_key=key)
+                    # explanation.add_layers(layer_names, saliencies[choice][reward_type], key=key)
                     group.add_bar(bar)
 
                 decomposed_q_chart.add_bar_group(group)
@@ -118,9 +116,9 @@ def run_task(evaluation_config, network_config, reinforce_config):
 
             action = env.new_action()
             action.attack_quadrant(tower_to_kill)
-            action.skip = False if  evaluation_config.render else True
+            action.skip = False if evaluation_config.render else True
 
-            state = env.act(action, explanation = explanation)
+            state = env.act(action, explanation=explanation)
 
             time.sleep(0.5)
 
@@ -128,7 +126,7 @@ def run_task(evaluation_config, network_config, reinforce_config):
 
         logger.info("Episode %d : %d, Step: %d" % (episode + 1, total_reward, step))
 
-        episode_summary.value.add(tag = "Test/Episode Reward", simple_value = total_reward)
+        episode_summary.value.add(tag="Test/Episode Reward", simple_value=total_reward)
         test_summary_writer.add_summary(episode_summary, episode + 1)
 
     test_summary_writer.flush()
